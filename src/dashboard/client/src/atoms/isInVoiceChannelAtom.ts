c1fedbc4faa614d538c559ef6f091c5a45721a74
NayZ <55506519+nayzflux@users.noreply.github.com>
2023-05-20 23:44:02 +0200

    fix login message not showing

--- src/dashboard/client/src/atoms/isInVoiceChannelAtom.ts
+++ src/dashboard/client/src/atoms/isInVoiceChannelAtom.ts
@@ -2,7 +2,7 @@ import {atom} from "recoil";
 
 const isInVoiceChannelState = atom({
     key: 'isInVoiceChannelState', // unique ID (with respect to other atoms/selectors)
-    default: true
+    default: false
 });
 
 export default isInVoiceChannelState;
