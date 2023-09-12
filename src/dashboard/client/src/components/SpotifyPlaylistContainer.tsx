d4afb7fe9b117861a59ce99479308ceb15c18002
NayZ <55506519+nayzflux@users.noreply.github.com>
2023-09-12 18:10:37 +0200

    fix client crash

--- src/dashboard/client/src/components/SpotifyPlaylistContainer.tsx
+++ src/dashboard/client/src/components/SpotifyPlaylistContainer.tsx
@@ -26,7 +26,7 @@ const SpotifyPlaylistContainer = () => {
         <div className="flex flex-row items-center space-x-3">
             {loggedWithSpotify ? spotifyPlaylists?.map((playlist) => {
                 return(
-                    <SpotifyPlaylistItem key={playlist.id} owner={playlist.owner.display_name} id={playlist.id} name={playlist.name} description={playlist.description} imageUrl={playlist.images[0].url} url={playlist.external_urls.spotify}/>
+                    <SpotifyPlaylistItem key={playlist.id} owner={playlist.owner.display_name} id={playlist.id} name={playlist.name} description={playlist.description} imageUrl={playlist?.images[0]?.url} url={playlist.external_urls.spotify}/>
                 )
             }) : <div className="w-full h-54 flex flex-col items-center justify-center space-y-3"><p className="font-bold text-lg">We can't retrieve your Spotify playlist!</p><p>You've not linked your Spotify Account yet</p><button className="rounded-lg font-semibold active:scale-95 transition-all ease-out duration-700 px-3 py-2 bg-green-500 hover:shadow-green-400 hover:shadow-2xl" onClick={(e) => router.push((process.env.NEXT_PUBLIC_API_URL || 'https://api.soundwave.nayz.fr/api') + '/spotify/login')}>Link Spotify Account</button></div>}
         </div>
